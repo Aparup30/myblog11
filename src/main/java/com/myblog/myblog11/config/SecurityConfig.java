9d51b9990c580ff54965248b62b0d16af7f793a5
Aparup30 <rupmondal30@gmail.com>
2024-02-06 16:24:39 +0530

    3/02/24

--- src/main/java/com/myblog/myblog11/config/SecurityConfig.java
+++ src/main/java/com/myblog/myblog11/config/SecurityConfig.java
@@ -32,6 +32,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers(HttpMethod.GET,"/api/**").permitAll()
+                .antMatchers(HttpMethod.POST,"/api/auth/**").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
