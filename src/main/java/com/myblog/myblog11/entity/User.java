f07011835ddc7346cf300fa98fbb2a18242938c7
Aparup30 <rupmondal30@gmail.com>
2024-02-08 15:41:38 +0530

    7/02/24

--- src/main/java/com/myblog/myblog11/entity/User.java
+++ src/main/java/com/myblog/myblog11/entity/User.java
@@ -3,6 +3,7 @@ package com.myblog.myblog11.entity;
 import lombok.Data;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Data
 @Entity
@@ -19,4 +20,11 @@ public class User {
     private String username;
     private String email;
     private String password;
+   // private String rolesType;
+
+    @ManyToMany(fetch = FetchType.EAGER,cascade = CascadeType.ALL)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id",referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id",referencedColumnName = "id"))
+    private Set<Role> roles;
 }
